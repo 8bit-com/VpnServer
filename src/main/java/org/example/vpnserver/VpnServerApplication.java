a4b2f26a73f3ec26cecc4ff21776a5f8bf3cc2b2
Vladimir <v.dementev@bftcom.com>
2026-06-07 17:50:40 +0700

    Initial commit

---
+++ src/main/java/org/example/vpnserver/VpnServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.vpnserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpnServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VpnServerApplication.class, args);
+    }
+
+}
