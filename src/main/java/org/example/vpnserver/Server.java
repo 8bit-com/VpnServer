bbfc7971cf331abb8e910c5394ee18a8af8d4599
Vladimir <v.dementev@bftcom.com>
2026-06-09 19:21:09 +0700

    fix rx

--- src/main/java/org/example/vpnserver/Server.java
+++ src/main/java/org/example/vpnserver/Server.java
@@ -111,7 +111,7 @@ public class Server {
     public ResponseEntity<byte[]> rx() throws InterruptedException {
         byte[] packet = priorityToClient.poll();
         if (packet == null) {
-            packet = normalToClient.poll(100, TimeUnit.MILLISECONDS);
+            packet = normalToClient.poll(5, TimeUnit.SECONDS);
         }
 
         if (packet == null) {
